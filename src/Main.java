2f43abb9952202dfed68f1e15ec8ed0f1fbf22e1
alexmm23 <alejandromontes393@gmail.com>
2024-10-20 20:27:05 -0600

    Add score label and player movement to scenario

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,10 @@ import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
-
         JFrame frame = new JFrame("Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
         frame.add(new Scenario());
         frame.setVisible(true);
-
     }
 }
--- src/Scenario.java
+++ src/Scenario.java
@@ -1,28 +1,51 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Scenario extends JPanel implements KeyListener {
+public class Scenario extends JPanel implements KeyListener, ActionListener {
     private Player player;
     private Obstacle[] obstacles;
     private int currentLane = 1; // 0, 1, 2
+    private JLabel scoreLabel;
+    private Timer timer;
+    private int score = 0;
+    private int width = 800;
+    private int height = 800;
+    private int speed = 10;
+
+
     public Scenario() {
         player = new Player("Player", 0, 0);
+        player.setLane(currentLane);
         obstacles = new Obstacle[10];
         for (int i = 0; i < obstacles.length; i++) {
-            obstacles[i] = new Obstacle(0, 0, 0);
+            obstacles[i] = new Obstacle(0, 50, 0);
         }
+        scoreLabel = new JLabel("Score: " + score);
+        timer = new Timer(1000, this);
+        timer.start();
+        add(scoreLabel);
         setFocusable(true);
         requestFocusInWindow();
         addKeyListener(this);
     }
-    public void update() {
-        player.move(0);
-        for (Obstacle obstacle : obstacles) {
-            obstacle.update();
+
+    public void updateGame() {
+        player.setLane(currentLane);
+        if (timer.isRunning() && score % 10 == 0) {
+            speed += 20;
         }
+        for (Obstacle obstacle : obstacles) {
+            obstacle.update(speed);
+        }
+        score += timer.getDelay() / 100;
+        scoreLabel.setText("Score: " + score);
+        repaint();
     }
+
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -32,13 +55,14 @@ public class Scenario extends JPanel implements KeyListener {
             obstacle.draw(g);
         }
     }
+
     public void drawLanes(Graphics g) {
         g.setColor(Color.BLACK);
-        g.fillRect(0, 0, 800, 600);
+        g.fillRect(0, 50, width, height);
         g.setColor(Color.WHITE);
-        g.fillRect(0, 0, 800, 100);
-        g.fillRect(0, 200, 800, 100);
-        g.fillRect(0, 400, 800, 100);
+        g.fillRect(0, 50, width, 100);
+        g.fillRect(0, 250, width, 100);
+        g.fillRect(0, 450, width, 100);
     }
 
     @Override
@@ -50,25 +74,38 @@ public class Scenario extends JPanel implements KeyListener {
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_UP:
-                player.move(0);
+                //jump
                 System.out.println("UP");
                 break;
             case KeyEvent.VK_RIGHT:
-                player.move(1);
+                if (currentLane < 2) {
+                    currentLane++;
+                }
+                player.setLane(currentLane);
                 System.out.println("RIGHT");
                 break;
             case KeyEvent.VK_DOWN:
-                player.move(2);
+                //player.move(2);
                 System.out.println("DOWN");
                 break;
             case KeyEvent.VK_LEFT:
-                player.move(3);
+                if (currentLane > 0) {
+                    currentLane--;
+                }
+                player.setLane(currentLane);
                 System.out.println("LEFT");
                 break;
         }
+        repaint();
     }
+
     @Override
     public void keyReleased(KeyEvent e) {
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        updateGame();
     }
 }
